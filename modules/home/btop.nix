e146723ca8c9e59db1cfc672296bca0a10ead1bb
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-04-09 00:03:47 -0700

    btop theme

--- modules/home/btop.nix
+++ modules/home/btop.nix
@@ -1,8 +1,14 @@
 _: {
-  flake.homeModules.btop = {
-    pkgs,
-    ...
-  }: {
+  flake.homeModules.btop = {pkgs, ...}: {
+    # catppuccin mocha with oled black background
+    xdg.configFile."btop/themes/catppuccin_mocha.theme".text = builtins.replaceStrings
+      [''theme[main_bg]="#1e1e2e"'']
+      [''theme[main_bg]="#000000"'']
+      (builtins.readFile (builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/btop/main/themes/catppuccin_mocha.theme";
+        sha256 = "0i263xwkkv8zgr71w13dnq6cv10bkiya7b06yqgjqa6skfmnjx2c";
+      }));
+
     programs.btop = {
       enable = true;
       package = pkgs.btop.override {cudaSupport = true;};
