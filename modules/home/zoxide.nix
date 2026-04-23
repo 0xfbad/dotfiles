e7549e069ee6ef7b39d164e6d7b6b88495e32996
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-04-23 15:24:10 -0700

    zoxide doctor kill

--- modules/home/zoxide.nix
+++ modules/home/zoxide.nix
@@ -1,16 +1,14 @@
 _: {
-  flake.homeModules.zoxide = {lib, ...}: {
+  flake.homeModules.zoxide = _: {
     programs.zoxide = {
       enable = true;
-      enableZshIntegration = false;
+      enableZshIntegration = true;
       options = [
         "--cmd"
         "cd"
       ];
     };
-    # zoxide must init after all other shell integrations to avoid its doctor warning
-    programs.zsh.initContent = lib.mkOrder 5000 ''
-      eval "$(zoxide init zsh --cmd cd)"
-    '';
+    # doctor warning nags about init order, we don't care
+    home.sessionVariables._ZO_DOCTOR = "0";
   };
 }
