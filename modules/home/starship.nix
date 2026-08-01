760387fe022dac13d7f04abe66a21d3993292267
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-08-01 15:39:49 -0400

    increase starship timeout

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -8,7 +8,7 @@ _: {
       enableZshIntegration = true;
       settings = {
         add_newline = true;
-        command_timeout = 200;
+        command_timeout = 2000;
         format = "$all";
         right_format = "";
 
