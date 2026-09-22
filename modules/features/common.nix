38572b1714525d30480466cbc43ddf0325e3f0cc
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-09-22 10:39:59 -0700

    rename virtcam

--- modules/features/common.nix
+++ modules/features/common.nix
@@ -95,7 +95,7 @@ in
       boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
       boot.kernelModules = [ "v4l2loopback" ];
       boot.extraModprobeConfig = ''
-        options v4l2loopback devices=1 video_nr=1 card_label="OBS Virtual Camera" exclusive_caps=1
+        options v4l2loopback devices=1 video_nr=1 card_label="Anker PowerConf C200" exclusive_caps=1
       '';
 
       systemd.services.generate-issue = {
